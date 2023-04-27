42b8bd9d9c618ad590308f9d97fb11cdb0ff83bb
GoldenArcher07 <115811159+GoldenArcher07@users.noreply.github.com>
2023-04-27 19:43:21 +0200

    removed unused variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import Signup from "./pages/sugnup/Signup";
 import Login from "./pages/login/Login";
 import "./App.css";
-import { useState } from "react";
 
 function App() {
-  const [user, setUser] = useState();
+  
   return (
     <div className="App">
       <Login></Login>
