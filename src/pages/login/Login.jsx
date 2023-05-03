4d9b411750a7266a031c7567612ff88dcf4c131d
branka arsic <arsicbranka72@gmail.com>
2023-05-03 12:37:01 +0200

    add login form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
-export default function Login() {
-  const displayEventAction = () => {
-    console.log("yeee, react is cool!!!");
+export default function Login(props) {
+  const displayEventAction = () => {};
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div onClick={displayEventAction} className="background">
-      <h1>This is Login component</h1>
+    <div onClick={displayEventAction} className="backgrounds">
+      <h1 className="naslov">Log in</h1>
+      <form className="logform" onSubmit={handleSubmit}>
+        <label className="lab" for="email">
+          E-mail:
+        </label>
+        <input
+          className="inp"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Enter your e-mail"
+        />
+
+        <label className="lab" for="password">
+          Password:
+        </label>
+        <input
+          className="inp"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type={passwordShown ? "text" : "password"}
+          placeholder="********"
+        />
+        <span onClick={togglePassword}>Show</span>
+      </form>
+      <button className="butt" type="submit">
+        Login
+      </button>
     </div>
   );
 }
